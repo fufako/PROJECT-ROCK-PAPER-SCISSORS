c5c262f5a4360f57b701b15dc28c40508daeb08b
fufako <patryko.mikiciuk@gmail.com>
2022-04-29 20:29:22 +0200

    Add gameScore function

--- script.js
+++ script.js
@@ -12,20 +12,51 @@ function playRound(){
     if (userChoice.toUpperCase() == 'ROCK' || userChoice.toUpperCase() == 'PAPER' || userChoice.toUpperCase() == 'SCISSORS' ){
         if (userChoice.toUpperCase() == computerChoice){
             document.getElementById("results").innerHTML = 'TIE!';
+            return 0
         } else if (userChoice.toUpperCase() == 'ROCK' && computerChoice == 'PAPER'){
             document.getElementById("results").innerHTML = 'YOU LOSE!';
+            return -1
         } else if (userChoice.toUpperCase() == 'ROCK' && computerChoice == 'SCISSORS'){
             document.getElementById("results").innerHTML = 'YOU WIN!';  
+            return 1
         } else if (userChoice.toUpperCase() == 'PAPER' && computerChoice == 'SCISSORS'){
             document.getElementById("results").innerHTML = 'YOU LOSE!';
+            return 0
         } else if (userChoice.toUpperCase() == 'PAPER' && computerChoice == 'ROCK'){
-            document.getElementById("results").innerHTML = 'YOU WIN!';       
+            document.getElementById("results").innerHTML = 'YOU WIN!';     
+            return 1  
         } else if (userChoice.toUpperCase() == 'SCISSORS' && computerChoice == 'ROCK'){
             document.getElementById("results").innerHTML = 'YOU LOSE!';
+            return 0
         } else if (userChoice.toUpperCase() == 'SCISSORS' && computerChoice == 'PAPER'){
             document.getElementById("results").innerHTML = 'YOU WIN!';
+            return 1
         } 
     }else{
         document.getElementById("results").innerHTML = 'Invalid input!';
     }
+}
+
+let pcWins = 0;
+let draws = 0;
+let myWins = 0;
+function gameScore(){
+    let result = playRound()
+    if (result == 1) {
+        myWins++;
+        document.getElementById("myWins").innerHTML = myWins;
+        return
+      } else if (result == 0) {
+        draws++;
+        document.getElementById('draws').innerHTML = draws;
+        return
+      } else {
+        pcWins++;
+        document.getElementById("pcWins").innerHTML = pcWins;
+        return
+      }
+      
+      
+      
+    
 }
