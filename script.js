68107c4b17bad267d20eb4fb20672a82f63c8abd
fufako <patryko.mikiciuk@gmail.com>
2022-05-17 15:49:00 +0200

    Add styles, script and html. All working

--- script.js
+++ script.js
@@ -1,62 +1,54 @@
-
-function computerPlay(){
-    let options = ['ROCK', 'PAPER', 'SCISSORS'];
-    let random = Math.floor(Math.random() * options.length);
-    return options[random];
+const weakStrong = {
+  rock: { weakTo: "paper", strongTo: "scissors" },
+  paper: { weakTo: "scissors", strongTo: "rock" },
+  scissors: { weakTo: "rock", strongTo: "paper" },
 }
-function playRound(){
-    let userChoice = document.getElementById('choiceField').value;
-    let computerChoice = computerPlay();
-    console.log(userChoice)
-    console.log(computerChoice)
-    if (userChoice.toUpperCase() == 'ROCK' || userChoice.toUpperCase() == 'PAPER' || userChoice.toUpperCase() == 'SCISSORS' ){
-        if (userChoice.toUpperCase() == computerChoice){
-            document.getElementById("results").innerHTML = 'TIE!';
-            return 0
-        } else if (userChoice.toUpperCase() == 'ROCK' && computerChoice == 'PAPER'){
-            document.getElementById("results").innerHTML = 'YOU LOSE!';
-            return -1
-        } else if (userChoice.toUpperCase() == 'ROCK' && computerChoice == 'SCISSORS'){
-            document.getElementById("results").innerHTML = 'YOU WIN!';  
-            return 1
-        } else if (userChoice.toUpperCase() == 'PAPER' && computerChoice == 'SCISSORS'){
-            document.getElementById("results").innerHTML = 'YOU LOSE!';
-            return 0
-        } else if (userChoice.toUpperCase() == 'PAPER' && computerChoice == 'ROCK'){
-            document.getElementById("results").innerHTML = 'YOU WIN!';     
-            return 1  
-        } else if (userChoice.toUpperCase() == 'SCISSORS' && computerChoice == 'ROCK'){
-            document.getElementById("results").innerHTML = 'YOU LOSE!';
-            return 0
-        } else if (userChoice.toUpperCase() == 'SCISSORS' && computerChoice == 'PAPER'){
-            document.getElementById("results").innerHTML = 'YOU WIN!';
-            return 1
-        } 
-    }else{
-        document.getElementById("results").innerHTML = 'Invalid input!';
-    }
+const weapons = ["rock", "paper", "scissors"]
+
+document.querySelectorAll("#player-choices > img").forEach((element) => {
+  element.addEventListener("click", (event) => {
+    const choice = event.target.id
+    const pcChoice = Math.floor(Math.random() * 3)
+    playRound(choice, pcChoice)
+    changeContent(choice, pcChoice)
+  })
+})
+function playGame() {}
+
+function playRound(choice, pcChoice) {
+  const result = document.getElementById("round-result")
+  let playerScore = 0
+  let pcScore = 0
+  if (weakStrong[choice].strongTo === weapons[pcChoice]) {
+    playerScore += 1
+    updateScoreBoard("player")
+    result.textContent = "You win!"
+  } else if (weakStrong[choice].weakTo === weapons[pcChoice]) {
+    pcScore += 1
+    updateScoreBoard("pc")
+    result.textContent = "You lose!"
+  } else {
+    console.log("tie")
+    result.textContent = "It's a tie!"
+  }
+}
+function changeContent(choice, pcChoice) {
+  const playerPick = document.getElementById("player-info")
+  const playerWeapon = document.getElementById("player-choices")
+  const pcPick = document.getElementById("pc-info")
+  const pcWeapon = document.getElementById("pc-choices")
+  playerWeapon.innerHTML = '<img src="img/' + choice + '.png"> '
+  playerPick.textContent = "You made your pick"
+  pcPick.textContent = "Opponent's pick"
+  pcWeapon.innerHTML = '<img src="img/' + weapons[pcChoice] + '.png"> '
 }
 
-let pcWins = 0;
-let draws = 0;
-let myWins = 0;
-function gameScore(){
-    let result = playRound()
-    if (result == 1) {
-        myWins++;
-        document.getElementById("myWins").innerHTML = myWins;
-        return
-      } else if (result == 0) {
-        draws++;
-        document.getElementById('draws').innerHTML = draws;
-        return
-      } else if (result== -1) {
-        pcWins++;
-        document.getElementById("pcWins").innerHTML = pcWins;
-        return
-      }
-      
-      
-      
-    
+function updateScoreBoard(winner) {
+  const playerPoints = document.getElementById("point")
+  const pcPoints = document.getElementById("pc-point")
+  if (winner == "player") {
+    playerPoints.src = "img/star_point.png"
+  } else if (winner == "pc") {
+    pcPoints.src = "img/star_point.png"
+  }
 }
